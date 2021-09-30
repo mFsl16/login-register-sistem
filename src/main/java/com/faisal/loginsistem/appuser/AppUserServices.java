f2bdc493ba6dc8975a96da9b560d61e7057b4523
mFsl16 <cd.muhammadfaisal@gmail.com>
2021-09-30 15:10:47 +0700

    30 sep 2021

--- src/main/java/com/faisal/loginsistem/appuser/AppUserServices.java
+++ src/main/java/com/faisal/loginsistem/appuser/AppUserServices.java
@@ -4,12 +4,19 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
 public class AppUserServices implements UserDetailsService {
 
+    private final AppUserRepository appUserRepository;
+    private final static String USER_NOT_FOUND = "user with email %s not found";
+
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         // TODO Auto-generated method stub
-        return null;
+        return appUserRepository.findByEmail(email).orElseThrow(() -> new UsernameNotFoundException(String.format(USER_NOT_FOUND, email)));
     }
-    
 }
